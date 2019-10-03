69581c92d051ddd4acf6bd942fc8af21ab7fcc02
Henrik Finsberg <henriknf@simula.no>
2019-10-03 12:36:16 +0200

    Update to new slack api

--- scholar_bot/post_slack.py
+++ scholar_bot/post_slack.py
@@ -1,28 +1,24 @@
 #!/usr/bin/env python3
 # coding: utf-8
-
-from slackclient import SlackClient
+import slack
 
 from .config import SLACK_TOKEN
 
 
 class SlackPoster:
     def __init__(self, token, channels):
-        self.client = SlackClient(token)
+        self.client = slack.WebClient(token=token)
         self.channels = channels
 
     def post(self, message):
         for ch in self.channels:
-            self.client.api_call(
-                'chat.postMessage',
-                channel=ch,
-                text=message,
-                as_user=False,
-                username='scholarbot'
+            self.client.chat_postMessage(
+                channel=ch, text=message, as_user=False, username="scholarbot"
             )
 
 
 _poster = None
+
 
 def post_to_slack(message, channels):
     global _poster
